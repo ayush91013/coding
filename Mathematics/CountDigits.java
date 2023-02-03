ab83ebcbcc4fd7856625a32fe22c3719bf413018
ayush91013 <ayush91013@gmail.com>
2023-02-03 19:28:16 +0530

    Counting_Digits

---
+++ Mathematics/CountDigits.java
@@ -0,0 +1,16 @@
+package Mathematics;
+import java.util.*;
+public class CountDigits {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int count;
+        System.out.println("Please Enter The Number: ");
+        int n = sc.nextInt();
+        count = 0;
+        while(n != 0){
+            count = count + 1;
+            n = n/10;
+        }
+        System.out.print(count);1234
+    }
+}
