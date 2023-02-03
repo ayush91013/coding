c726d4881ab5f6772e3a5b1c5d6526573cfe1ce5
ayush91013 <ayush91013@gmail.com>
2023-02-03 20:12:42 +0530

    Counting_Digits_recursive

--- Mathematics/CountDigits.java
+++ Mathematics/CountDigits.java
@@ -3,10 +3,9 @@ import java.util.*;
 public class CountDigits {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int count;
+        int count=0;
         System.out.println("Please Enter The Number: ");
         int n = sc.nextInt();
-        count = 0;
         while(n != 0){
             count = count + 1;
             n = n/10;
--- Mathematics/CountDigitsRecursive.java
+++ Mathematics/CountDigitsRecursive.java
@@ -1,4 +1,19 @@
 package Mathematics;
-
+import java.util.*;
 public class CountDigitsRecursive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count=0;
+        System.out.println("Please Enter The Number: ");
+        int k = sc.nextInt();
+        int result = Count(k);
+        System.out.print(result);
+
+    }
+    public static int Count(int n){
+        if(n==0){
+            return 0;
+        }
+        return 1 + Count(n/10);
+    }
 }
