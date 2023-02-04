bfbddfde37a611b3eafb2069811bdb820f78da80
ayush91013 <ayush91013@gmail.com>
2023-02-04 21:30:13 +0530

    Mathematics

---
+++ Mathematics/Divisors.java
@@ -0,0 +1,19 @@
+package Mathematics;
+
+import java.util.Scanner;
+
+public class Divisors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter the Number: ");
+        int n = sc.nextInt();
+        for(int i=1; i*i<n;i++){
+            if(n%i==0){
+                System.out.println(i);
+                if(i != n/i){
+                    System.out.println(n/i);
+                }
+            }
+        }
+    }
+}
