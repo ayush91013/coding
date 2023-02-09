8094ad7661f5a9490acd43a5347669801b4ffcbb
ayush91013 <ayush91013@gmail.com>
2023-02-09 15:43:40 +0530

    Mathematics_Problems

---
+++ Mathematics/Problems/cTof.java
@@ -0,0 +1,13 @@
+package Mathematics.Problems;
+
+import java.util.Scanner;
+
+public class cTof {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        double f;
+        f = (c*1.8) + 32;
+        System.out.print(f);
+    }
+}
