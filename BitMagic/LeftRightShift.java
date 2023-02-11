c512d3b92683c6895da19fab437a5c178a20daa7
ayush91013 <ayush91013@gmail.com>
2023-02-11 17:38:53 +0530

    BitwiseOperations

---
+++ BitMagic/LeftRightShift.java
@@ -0,0 +1,17 @@
+package BitMagic;
+
+import java.util.Scanner;
+
+public class LeftRightShift {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter the Number: ");
+        int a = sc.nextInt();
+        System.out.println("Please Enter the Shift Number: ");
+        int b = sc.nextInt();
+        System.out.println("Left Shift");
+        System.out.println(a<<b);
+        System.out.println("Right Shift");
+        System.out.println(a>>b);
+    }
+}
