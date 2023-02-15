937e0b57a54ebf94aaef414d781a2cbb5fcd40f7
ayush91013 <ayush91013@gmail.com>
2023-02-15 11:08:35 +0530

    Power_of_2

--- BitMagic/PowerOf2.java
+++ BitMagic/PowerOf2.java
@@ -7,5 +7,15 @@ public class PowerOf2 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please Enter the Number: ");
         int a = sc.nextInt();
-        int count =0;
+//        double c = (Math.log(a))/(Math.log(2));
+//        int b = (int) c;
+//        double d = Math.pow(2,b);
+//        long e = (long) d;
+        if((a&(a-1))==0){
+            System.out.print("Power of 2");
+        }
+        else{
+            System.out.print("Not pof 2");
+        }
+    }
 }
