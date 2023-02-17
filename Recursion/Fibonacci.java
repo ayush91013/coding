5c5bb39266daa0171f7eed7eb60037ac764a87f8
ayush91013 <ayush91013@gmail.com>
2023-02-18 04:30:25 +0530

    Fibonacci Numbers for n>=0

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+import java.awt.*;
+
+public class Fibonacci {
+    // n>=0 Fibonacci Series = 0,1,1,2,3,5........
+    static int Fib(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return Fib(n-1)+Fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        int c = Fib(5);
+        System.out.println(c);
+    }
+}
