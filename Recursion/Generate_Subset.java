8f93ca4f34e628416d7c460e50aa23651b712fe6
ayush91013 <ayush91013@gmail.com>
2023-02-18 05:51:32 +0530

    Generating subsets

--- Recursion/Generate_Subset.java
+++ Recursion/Generate_Subset.java
@@ -1,4 +1,16 @@
 package Recursion;
-
+import java.lang.String;
 public class Generate_Subset {
+    static void fun(String s,String curr, int i){
+        if(i==s.length()){
+            System.out.println(curr+ " ");
+            return;
+        }
+        fun(s,curr,i+1);
+        fun(s,curr+s.charAt(i),i+1);
+    }
+
+    public static void main(String[] args) {
+        fun("abc", "",0);
+    }
 }
